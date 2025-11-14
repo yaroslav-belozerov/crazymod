77cbb7a6b4e0f032125269291fdbda81fb586694
Iaroslav Belozerov <iarbelozerov@gmail.com>
2025-11-14 22:26:24 +0100

    init

---
+++ src/client/java/tech/tarakoshka/mymod/MymodDataGenerator.java
@@ -0,0 +1,11 @@
+package tech.tarakoshka.mymod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MymodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
